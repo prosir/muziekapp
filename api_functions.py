5201e4cfb08f3c63f7a3da248a7107f0b5941afa
Jacco <34662810+prosir@users.noreply.github.com>
2024-09-17 15:01:49 +0200

    updated the code!
    Added a list of translations this was generated online the list. Because the app is in dutch we needed to fix this :)

---
+++ api_functions.py
@@ -0,0 +1,59 @@
+import requests
+from config import API_URI, API_KEY
+
+
+def get_top_tracks(artist_name, API_KEY):
+    params = {
+        'method': 'artist.getTopTracks',
+        'artist': artist_name,
+        'api_key': API_KEY,
+        'format': 'json',
+        'limit': 5
+    }
+    response = requests.get(API_URI, params=params)
+    if response.status_code == 200:
+        data = response.json()
+
+    if 'toptracks' in data:
+        top_tracks = data['toptracks']['track']
+        return [track['name'] for track in top_tracks]
+
+def get_artist_info(artist_name, API_KEY):
+    params = {
+        'method': 'artist.getInfo',
+        'artist': artist_name,
+        'api_key': API_KEY,
+        'format': 'json'
+    }
+
+    response = requests.get(API_URI, params=params)
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print(f"Error: Status code {response.status_code}")
+
+    if 'artist' in data:
+        artist_info = data['artist']
+        artist_details = {
+            'artist_name': artist_info['name'],
+            'artist_genres': [tag['name'] for tag in artist_info['tags']['tag']]
+        }
+        return artist_details
+
+def get_top_artists_by_country(country, API_KEY):
+    params = {
+        'method': 'geo.getTopArtists',
+        'country': country,
+        'api_key': API_KEY,
+        'format': 'json',
+        'limit': 10
+    }
+    response = requests.get(API_URI, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'topartists' in data:
+            top_artists = data['topartists']['artist']
+            return [artist['name'] for artist in top_artists]
+    return []
+
--- muziekapp.py
+++ muziekapp.py
@@ -1,22 +1,21 @@
 from config import API_KEY
-from toptracks import get_top_tracks
-from artistinfo import get_artist_info
-from favorite_artist import load_favorite_artists, add_favorite_artist
+from api_functions import *
+from country_translate import *
 
 
 def muziekapp_menu():
-    favorite_artists = load_favorite_artists()
     while True:
         print("\n##########################################")
         print("#          Het Muziekapp Menu            #")
         print("##########################################")
         print("# 1. Informatie genres opvragen artiest  #")
         print("# 2. Top 5 tracks opvragen artiest       #")
-        print("# 3. Afsluiten                           #")
+        print("# 3. Top 10 artiesten per land opvragen  #")
+        print("# 4. Afsluiten                           #")
         print("##########################################\n")
-    input_user = input("Voer een keuze in (1, 2, 3): ")
+        input_user = input("Voer een keuze in (1, 2, 3, 4, 5): ")
 
-      if input_user == "1":
+        if input_user == "1":
             artist_input = input("Voer de naam van de artiest in: ")
             info = get_artist_info(artist_input, API_KEY)
             if not info:
@@ -24,6 +23,7 @@ def muziekapp_menu():
             else:
                 print(f"Naam: {info['artist_name']}")
                 print(f"Genres: {', '.join(info['artist_genres'])}")
+
         elif input_user == "2":
             artist_input = input("Voer de naam van de artiest in: ")
             top_tracks = get_top_tracks(artist_input, API_KEY)
@@ -33,10 +33,21 @@ def muziekapp_menu():
                 print(f"Best beluisterde nummers voor {artist_input}:")
                 for index, track in enumerate(top_tracks, start=1):
                     print(f"{index}. {track}")
+
         elif input_user == "3":
+            country_input = input("Voer de naam van het land in: ")
+            translated_country = translate_country_name(country_input)
+            top_artists = get_top_artists_by_country(translated_country, API_KEY)
+            if not top_artists:
+                print(f"Geen gevonden top artiesten voor het land {country_input}.")
+            else:
+                print(f"Best beluisterde artiesten voor {country_input}:")
+                for index, artist in enumerate(top_artists, start=1):
+                    print(f"{index}. {artist}")
+
+        elif input_user == "4":
             print("Het programma wordt afgesloten.")
             break
 
         else:
             print("Ongeldige keuze, probeer het opnieuw.")
-
